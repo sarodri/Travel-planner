a9461f9e568fe3ca70c464a531165f4c6f6be7c8
sarodri <sandra.ropero1987@gmail.com>
2025-01-17 11:10:15 +0100

    config and pages

--- travel-planner/src/main.jsx
+++ travel-planner/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Destinations from './pages/Destinations';
+import Itinerary from './pages/Itinerary';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/destinations" element={<Destinations />} />
+        <Route path="/itinerary" element={<Itinerary />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
---
+++ travel-planner/src/services/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://restcountries.com/v3.1';
+
+export const getAllCountries = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/all`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching countries:', error);
+    throw error;
+  }
+};
